c8cff07b46686c69f7a80f93519fbfb432677fb3
Julio Cesar Melegari <juliocesar.melegari@gmail.com>
2022-04-29 10:36:47 -0300

    limitando a visualizacao de posts apenas para seguidores

--- src/main/java/org/jcm/PostResource.java
+++ src/main/java/org/jcm/PostResource.java
@@ -6,15 +6,13 @@ import org.jcm.dto.CreatePostRequest;
 import org.jcm.dto.PostResponse;
 import org.jcm.model.Post;
 import org.jcm.model.User;
+import org.jcm.repository.FollowerRepository;
 import org.jcm.repository.PostRepository;
 import org.jcm.repository.UserRepository;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.stream.Collectors;
 
@@ -23,12 +21,14 @@ public class PostResource {
 
     private UserRepository userRepository;
     private PostRepository postRepository;
+    private FollowerRepository followerRepository;
 
     @Inject
-    public PostResource(UserRepository userRepository, PostRepository postRepository){
+    public PostResource(UserRepository userRepository, PostRepository postRepository, FollowerRepository followerRepository){
 
         this.userRepository = userRepository;
         this.postRepository = postRepository;
+        this.followerRepository = followerRepository;
     }
 
     @POST
@@ -46,11 +46,34 @@ public class PostResource {
     }
 
     @GET
-    public Response listPosts(@PathParam("userId") Long userId){
+    public Response listPosts(
+            @PathParam("userId") Long userId,
+            @HeaderParam("followerId") Long followerId){
         User user = userRepository.findById(userId);
         if(user == null){
             return Response.status(Response.Status.NOT_FOUND).build();
         }
+        if(followerId == null){
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("You forgot the header followerId")
+                    .build();
+        }
+        User follower = userRepository.findById(followerId);
+        if(follower ==null){
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("Inexistent followerId")
+                    .build();
+        }
+
+        boolean follows = followerRepository.follows(follower,user);
+        if(!follows){
+            return Response.status(Response.Status.FORBIDDEN)
+                    .entity("You can't see these posts")
+                    .build();
+        }
+
         PanacheQuery<Post> query = postRepository.find("user", Sort.by("dataTime", Sort.Direction.Descending) , user);
         var list = query.list();
         var postResponseList = list.stream()
